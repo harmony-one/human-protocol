90182a751fe9b1b8be96b91e5768b99c0014f4d1
potvik <yuriy@harmony.one>
2024-02-09 01:46:22 +0300

    Added Firebase

--- client/src/api/worker.ts
+++ client/src/api/worker.ts
@@ -1,40 +1,49 @@
 import axios from 'axios'
 import {UserAction} from "../types";
+import { firebaseClient } from '../firebase';
 
 const WorkerURL = 'https://kv-dev-message.humanprotocol.workers.dev'
 
 export const postUserTopics = async (address: string, topics: string[]) => {
-  const body = JSON.stringify({
+  return await firebaseClient.addUser({
     id: address,
     topics
   })
-  const { data } = await axios.post(
-    `${WorkerURL}/users`,
-    body,
-    {
-      headers: {
-        "Content-Type": "application/json"
-      }
-  })
-  return data
+
+  // const body = JSON.stringify({
+  //   id: address,
+  //   topics
+  // })
+  // const { data } = await axios.post(
+  //   `${WorkerURL}/users`,
+  //   body,
+  //   {
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  // })
+  // return data
 }
 
 export const getUserTopics = async (address: string): Promise<string[]> => {
-  const { data } = await axios.get(`${WorkerURL}/users/${address}`, {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  return data
+  return (await firebaseClient.getUser(address) as any).topics
+  // const { data } = await axios.get(`${WorkerURL}/users/${address}`, {
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // })
+  // return data
 }
 
 export const getUserActions = async (): Promise<UserAction[]> => {
-  const { data } = await axios.get(`${WorkerURL}/actions`, {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  return data
+  return await firebaseClient.getActions() as any
+
+  // const { data } = await axios.get(`${WorkerURL}/actions`, {
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // })
+  // return data
 }
 
 export interface SendUserActionParams {
@@ -45,15 +54,16 @@ export interface SendUserActionParams {
 }
 
 export const sendUserAction = async (params: SendUserActionParams): Promise<UserAction[]> => {
-  const body = JSON.stringify(params)
-  const { data } = await axios.post(
-    `${WorkerURL}/actions`,
-    body,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      }
-    })
-  return data
+  return await firebaseClient.addAction(params) as any
+  // const body = JSON.stringify(params)
+  // const { data } = await axios.post(
+  //   `${WorkerURL}/actions`,
+  //   body,
+  //   {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "Accept": "application/json"
+  //     }
+  //   })
+  // return data
 }
---
+++ client/src/firebase/index.ts
@@ -0,0 +1,86 @@
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, Firestore, setDoc, doc, query, where, WhereFilterOp, getDoc, limitToLast, orderBy } from 'firebase/firestore/lite';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBYDneFqjVr426MgnTiuwfgQO2nc-YYLn4",
+    authDomain: "human-protocol-413614.firebaseapp.com",
+    projectId: "human-protocol-413614",
+    storageBucket: "human-protocol-413614.appspot.com",
+    messagingSenderId: "202612542693",
+    appId: "1:202612542693:web:30c2c9fdbc4166de5c180c",
+    measurementId: "G-3VB7TC31EC"
+};
+
+interface GetListParams {
+    filter?: { key: string, value: any, operation: WhereFilterOp },
+    limit?: number;
+    page?: number;
+}
+
+class FirebaseClient {
+    app: FirebaseApp;
+    db: Firestore;
+
+    constructor() {
+        // Initialize Firebase
+        this.app = initializeApp(firebaseConfig);
+        this.db = getFirestore(this.app);
+
+        //const analytics = getAnalytics(app);
+    }
+
+    getList = async (collectionName: string, params?: GetListParams) => {
+        const { limit = 100, page = 0, filter } = params || {};
+
+        const col = collection(this.db, collectionName);
+
+        const snapshot = await getDocs(
+            filter ?
+                query(
+                    col,
+                    where(filter.key, filter.operation, filter.value),
+                    orderBy('created', 'asc'),
+                    limitToLast(limit)
+                ) :
+                query(
+                    col,
+                    orderBy('created', 'asc'),
+                    limitToLast(limit)
+                )
+        );
+
+        const list = snapshot.docs.map(doc => doc.data());
+
+        return list;
+    }
+
+    addUser = async (data: any) => {
+        await setDoc(doc(this.db, "users", data.id), {
+            ...data,
+            created: Math.floor(Date.now() / 1000)
+        });
+    }
+
+    addAction = async (data: any) => {
+        await setDoc(doc(this.db, "actions", data.id), {
+            ...data,
+            created: Math.floor(Date.now() / 1000)
+        });
+    }
+
+    getUser = async (id: string) => {
+        const snapshot = await getDoc(doc(this.db, "users", id))
+        return snapshot.data();
+    }
+
+    getUsers = (params?: GetListParams) => this.getList('users', params);
+
+    getActions = (params?: GetListParams) => this.getList('actions', params);
+}
+
+export const firebaseClient = new FirebaseClient();
